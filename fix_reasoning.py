c48a561e90d1234184c4a1d2530ab9540f84984f
fortunto2 <info@life2film.com>
2026-03-22 13:45:27 +0300

    chore: remove temporary patch scripts

--- fix_reasoning.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-with open('src/resources/chat/mod.rs', 'r') as f:
-    content = f.read()
-
-# I put it inside the struct but maybe inside the impl block incorrectly? Let's check where it went.
